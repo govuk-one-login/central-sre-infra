96ad0f6526c643990bf8ddcc140cfec29bd7c5f0
melvabout <andrew.melvin@digital.cabinet-office.gov.uk>
2024-04-08 10:52:55 +0100

    Changes to signiing profiles and deployment workflow

--- environments/staging/main.tf
+++ environments/staging/main.tf
@@ -30,8 +30,8 @@ module "pipelines" {
     allowed_accounts = "730335525541,572704362959"
     environment = "staging"
     include_promotion = "Yes"
-    signing_profile_arn = "arn:aws:signer:eu-west-2:891376909120:/signing-profiles/SigningProfile_jIC143lYZbgR"
-    signing_profile_version_arn = "arn:aws:signer:eu-west-2:891376909120:/signing-profiles/SigningProfile_jIC143lYZbgR/OEjYWMQ1LP"
-    artifact_source_bucket_arn = "arn:aws:s3:::centralised-logging-pipeli-artifactpromotionbucket-xkftvu95t3po"
-    artifact_source_bucket_event_trigger_role_arn = "arn:aws:iam::891376909120:role/PL-centralised-logging-pipeline-PromoTrigRole-061bd8c6c75f"
+    signing_profile_arn = "arn:aws:signer:eu-west-2:891376909120:/signing-profiles/SigningProfile_WOWPkr83gjpe"
+    signing_profile_version_arn = "arn:aws:signer:eu-west-2:891376909120:/signing-profiles/SigningProfile_WOWPkr83gjpe/VYpUeFWxU1"
+    artifact_source_bucket_arn = "arn:aws:s3:::centralised-logging-pipeli-artifactpromotionbucket-onqq1idmpmqy"
+    artifact_source_bucket_event_trigger_role_arn = "arn:aws:iam::891376909120:role/PL-centralised-logging-pipeline-PromoTrigRole-06b0771b5f2d"
 }
